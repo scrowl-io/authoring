eb382ab7bd708e28c539cc888dbd28122ad10336
aubeOSG <caube@osg.ca>
2022-10-11 14:19:39 -0400

    WIP - foundation

---
+++ config/src/typings/index.d.ts
@@ -0,0 +1,36 @@
+/**
+ * Global ambient declaration of various non javascript assets being imported into JS/TS
+ */
+declare module "*.css";
+
+declare module "*.scss";
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.md" {
+  const src: string;
+  export default src;
+}
