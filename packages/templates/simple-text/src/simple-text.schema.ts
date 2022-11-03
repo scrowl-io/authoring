597c5182e42bcc89c260877f9671f839ed8398ef
clarkeOSG <104869980+clarkeOSG@users.noreply.github.com>
2022-11-03 16:15:39 -0400

    better animation demo with staggered text

--- packages/templates/simple-text/src/simple-text.schema.ts
+++ packages/templates/simple-text/src/simple-text.schema.ts
@@ -12,7 +12,7 @@ export const SimpleTextSchema: SimpleTextSchemaProps = {
       type: 'Textbox',
       label: 'Text',
       value:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur!',
+        '# Starting \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur!',
       placeholder: 'Enter your text',
       multiLine: true,
       lines: 10,
