60784f4996ffd8bbdd67fd5c7121c3391d986e59
aubeOSG <caube@osg.ca>
2022-11-04 11:10:27 -0400

    Update confetti.tsx

--- packages/editor/src/app/components/animations/confetti/confetti.tsx
+++ packages/editor/src/app/components/animations/confetti/confetti.tsx
@@ -4,14 +4,10 @@ import { Lottie } from '../lottie';
 import { Error } from '../../error';
 
 export const Confetti = (props) => {
-  const loop = props.loop === false ? false : true;
-
   return (
-    <div {...props}>
-      <Error>
-        <Lottie animationData={animationData} loop={loop} />
-      </Error>
-    </div>
+    <Error>
+      <Lottie animationData={animationData} {...props} />
+    </Error>
   );
 };
 
