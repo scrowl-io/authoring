1a28c78c7aee9f3544eda73c177238ca02bdb8b4
aubeOSG <caube@osg.ca>
2022-11-10 23:15:48 -0500

    WIP - disabling block text as it is currently not working as intended

--- packages/player/dev/project.ts
+++ packages/player/dev/project.ts
@@ -48,7 +48,7 @@ export const create = () => {
     ],
     slides: [
       createSlide('Slide 1.1', 0, 0, 2, 'lessonIntro'),
-      createSlide('Slide 1.2', 0, 0, 0, 'blockText'),
+      // createSlide('Slide 1.2', 0, 0, 0, 'blockText'),
       createSlide('Slide 1.3', 0, 0, 1, 'simpleText'),
       createSlide('Slide 1.4', 0, 0, 3, 'twoColumn'),
     ],
