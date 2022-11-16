fe765ccf85416ae633e6b1d65a1a98066e495850
aubeOSG <caube@osg.ca>
2022-11-16 10:49:37 -0500

    update - export position determines app bar position

--- packages/editor/src/main/services/menu/items/index.ts
+++ packages/editor/src/main/services/menu/items/index.ts
@@ -1,7 +1,7 @@
-export * as menuItemEdit from './menu-items-edit';
 export * as menuItemFile from './menu-items-file';
-export * as menuItemHelp from './menu-items-help';
+export * as menuItemEdit from './menu-items-edit';
 export * as menuItemOutline from './menu-items-outline';
 export * as menuItemPreview from './menu-items-preview';
 export * as menuItemPublish from './menu-items-publish';
 export * as menuItemWindow from './menu-items-window';
+export * as menuItemHelp from './menu-items-help';
