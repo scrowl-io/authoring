f38bb7fed816efe5294229486893b91c6d51a2a4
aubeOSG <caube@osg.ca>
2022-11-24 12:39:46 -0500

    update - able to create project from start page

--- packages/editor/src/app/pages/start/components/start-new.tsx
+++ packages/editor/src/app/pages/start/components/start-new.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
+import { Icon, Button } from '@owlui/lib';
 import * as css from '../_page-start.scss';
 import { StartNewProps } from '../page-start.types';
-import { Icon, Button } from '@owlui/lib';
+import { Projects } from '../../../models';
+import { menu } from '../../../services';
+import { Workspace } from '../../';
 
 export const StartNew = ({ hasProjects, ...props }: StartNewProps) => {
-  const handleNewProject = () => {};
+  const navigator = useNavigate();
+  const inProgress = useRef(false);
+  const isReady = useRef(false);
+
+  const handleNewProject = () => {
+    if (inProgress.current) {
+      return;
+    }
+
+    inProgress.current = true;
+    Projects.create().then((result) => {
+      if (result.error) {
+        console.error(result);
+        return;
+      }
+
+      menu.API.enableProjectActions().then(() => {
+        inProgress.current = false;
+        navigator(Workspace.Path);
+      });
+    });
+  };
+
+  useEffect(() => {
+    if (isReady.current) {
+      return;
+    }
+
+    menu.API.disableProjectActions().then(() => {
+      isReady.current = false;
+    });
+
+    return () => {
+      isReady.current = true;
+    };
+  }, [isReady.current]);
 
   return (
     <div className={css.startSection} {...props}>
