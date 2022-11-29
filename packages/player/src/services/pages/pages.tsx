45f014ae3a9b5fb1577961577e5ec878a8427313
aubeOSG <caube@osg.ca>
2022-11-29 00:25:35 -0500

    lint

--- packages/player/src/services/pages/pages.tsx
+++ packages/player/src/services/pages/pages.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { PageDefinition, PageProps } from './pages.types';
 import {
