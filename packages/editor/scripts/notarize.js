819d320d1b5e429ac0274461006a43943c0623a7
aubeOSG <caube@osg.ca>
2022-12-20 11:07:09 -0500

    update - reseting use of secrets

--- packages/editor/scripts/notarize.js
+++ packages/editor/scripts/notarize.js
@@ -3,10 +3,6 @@ const dotenv = require("dotenv");
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
-  const log = process.stdout.write;
-
-  log(`process.env: ${JSON.stringify(process.env, null, 2)}`);
-  log(`process.env link: ${process.env.CSC_LINK}`);
 
   if (electronPlatformName !== "darwin") {
     return;
