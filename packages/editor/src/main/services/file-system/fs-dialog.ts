fd7cf31f6588f318fe68a2df6788c2766a8738c0
clarkeOSG <104869980+clarkeOSG@users.noreply.github.com>
2023-01-13 12:06:25 -0500

    pass browserWindow to message box to make modal

--- packages/editor/src/main/services/file-system/fs-dialog.ts
+++ packages/editor/src/main/services/file-system/fs-dialog.ts
@@ -35,7 +35,7 @@ export const message = (ev: rq.RequestEvent, options: MessageBoxOptions) => {
     if (!options) {
       resolve({
         error: true,
-        message: 'Unable to open dialog: options missing'
+        message: 'Unable to open dialog: options missing',
       });
       return;
     }
@@ -43,50 +43,63 @@ export const message = (ev: rq.RequestEvent, options: MessageBoxOptions) => {
     const type = options.type || 'warning';
     const buttons = options.buttons || ['Ok'];
     const defaultId = options.defaultId || 0;
-    
-    dialog.showMessageBox({
-      ...options,
-      type,
-      buttons,
-      defaultId,
-    }).then((res) => {
-      resolve({
-        error: false,
-        data: res,
-      });
-    }).catch((e) => {
-      resolve({
-        error: true,
-        message: 'Message Dialog Failed',
-        data: {
-          trace: e,
-        },
-      });
-    })
+    let openWin: BrowserWindow | null = null;
+    const openEvent = ev as IpcMainInvokeEvent;
+
+    if (openEvent.sender) {
+      openWin = BrowserWindow.fromWebContents(openEvent.sender);
+    }
+
+    if (openWin) {
+      dialog
+        .showMessageBox(openWin, {
+          ...options,
+          type,
+          buttons,
+          defaultId,
+        })
+        .then((res) => {
+          resolve({
+            error: false,
+            data: res,
+          });
+        })
+        .catch((e) => {
+          resolve({
+            error: true,
+            message: 'Message Dialog Failed',
+            data: {
+              trace: e,
+            },
+          });
+        });
+    }
   });
 };
 
 export const save = (ev: rq.RequestEvent, options: SaveDialogOptions) => {
   return new Promise<rq.ApiResult>((resolve) => {
-    dialog.showSaveDialog(options).then(({ canceled, filePath }) => {
-      resolve({
-        error: false,
-        data: {
-          canceled,
-          filePath,
-        },
+    dialog
+      .showSaveDialog(options)
+      .then(({ canceled, filePath }) => {
+        resolve({
+          error: false,
+          data: {
+            canceled,
+            filePath,
+          },
+        });
+      })
+      .catch((e) => {
+        resolve({
+          error: true,
+          message: 'Save dialog failed: unexpected',
+          data: {
+            trace: e,
+            options,
+          },
+        });
       });
-    })
-    .catch((e) => {
-      resolve({
-        error: true,
-        message: 'Save dialog failed: unexpected',
-        data: {
-          trace: e,
-          options,
-        },
-      });
-    })
   });
 };
 
