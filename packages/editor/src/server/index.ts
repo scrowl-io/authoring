5f61b819a4472685e50791e217952fb56d590350
aubeOSG <caube@osg.ca>
2023-01-18 14:46:48 -0500

    update - docker setup so that the container updates with host files

--- packages/editor/src/server/index.ts
+++ packages/editor/src/server/index.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
@@ -16,5 +19,10 @@ api.init(app);
 routes.init(app);
 
 app.listen(port, () => {
+  console.log('db config');
+  console.log('port:', process.env.DBPORT);
+  console.log('host:', process.env.DBHOST);
+  console.log('user:', process.env.DBUSER);
+  console.log('pass:', process.env.DBPASS);
   console.info(`Scrowl Web Server running at http://localhost:${port}/app`);
 });
